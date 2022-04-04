db8efb01e761be9354b37066c8e329507ed21125
justprzy <68716257+justprzy@users.noreply.github.com>
2022-04-04 10:56:49 -0600

    tiles

--- Bullet.gd
+++ Bullet.gd
@@ -12,10 +12,16 @@ func _physics_process(delta):
 	position += transform.x * speed * delta
 
 func _on_Bullet_area_entered(area):
-	if area.is_in_group("hitbox"):
+	if area.is_in_group("hitbox") || area.is_in_group("tile"):
 		yield(get_tree().create_timer(0.01), "timeout")
 		queue_free()
 
 #func remove():
 	#yield(get_tree().create_timer(0.01), "timeout")
 	#queue_free()
+
+
+func _on_BulletP1_body_entered(body):
+	if body.is_in_group("tile"):
+		yield(get_tree().create_timer(0.01), "timeout")
+		queue_free()
--- GunPickUp.gd
+++ GunPickUp.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
 func _on_Area2D_area_entered(area):
-	yield(get_tree().create_timer(0.1), "timeout")
-	queue_free()
+	if area.is_in_group("hitbox"):
+		yield(get_tree().create_timer(0.1), "timeout")
+		queue_free()
